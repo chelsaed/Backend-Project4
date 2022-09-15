9d3366211a8a77dc2d247125aca28021c4c86eec
chelsaed <102196325+chelsaed@users.noreply.github.com>
2022-09-15 18:28:11 -0500

    working code

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,66 +1,51 @@
-const User = require('../models/user')
+const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+const SECRET = process.env.SECRET;
 
-let create = (req, res) => {
-    User.create(req.body, (err, u)=>{
-        if(err){
-            res.status(400).json(err)
-            return
-        }
-            // res.render('profile', {u})
-        res.json({u, message: 'Yay you created one'})
-    })
-}
-
-const index = (req, res) => {
-    // respond with our bookmarks
-    User.find({}, (err, users)=> {
-        if(err){
-            res.status(400).json(err)
-            return
-        }
-        // res.render('home', {bookmarks})
-        res.json(users)
-    })
-    // .then(data => { res.json(data)})
-}
-
-let show = (req, res) => {
-    User.findById(req.params.id, (err, u) =>{
-        if(err){
-            res.status(400).json(err)
-            return
-        }
-
-        res.json(u)
-    })
-}
-
-let update = (req, res) => {
-    // Update user and send it back
-    User.findByIdAndUpdate(req.params.id, req.body, {new: true} ,(err, u) =>{
-        if(err){
-            res.status(400).json(err)
-            return
-        }
-        // send created user back as JSON
-        res.json(u)
-    } )
-}
-
-let  deleteIt= (req, res) => {
-    User.findByIdAndDelete(req.params.id, (err, u)=>{
-        if(err){
-            res.status(400).json(err)
-            return
-        }
-
-        res.json({message: 'Item Deleted'})
-    })
-}
 module.exports = {
-    create,
-    deleteIt,
-    update,
-    index,
-    show
+  signup,
+  login
+};
+
+async function signup(req, res) {
+  const user = new User(req.body);
+  try {
+    await user.save();
+    const token = createJWT(user);
+    res.json({ token });
+    
+
+  } catch (err) {
+    // Probably a duplicate email
+    
+    res.status(400).json(err);
+
+  } return res.redirect('/api/users');
+}
+
+async function login(req, res) {
+  try {
+    const user = await User.findOne({email: req.body.email});
+    if (!user) return res.status(401).json({err: 'bad credentials'});
+    user.comparePassword(req.body.pw, (err, isMatch) => {
+      if (isMatch) {
+        const token = createJWT(user);
+        res.json({token});
+      } else {
+        return res.status(401).json({err: 'bad credentials'});
+      }
+    });
+  } catch (err) {
+    return res.status(401).json(err);
+  }
+}
+
+/*----- Helper Functions -----*/
+
+function createJWT(user) {
+  return jwt.sign(
+    {user}, // data payload
+    SECRET,
+    {expiresIn: '24h'}
+  );
 }
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../../models/user');
+//const User = require('../../models/user');
 const usersCtrl = require('../../controllers/userController');
 
 /*---------- Public Routes ----------*/
-//router.post('/signup', usersCtrl.signup);
-//router.post('/login', usersCtrl.login);
+router.post('/signup', usersCtrl.signup);
+router.post('/login', usersCtrl.login);
+//router.get('/:id', usersCtrl.show);
 
 
 /*---------- Protected Routes ----------*/
