918e42b884efffa99c9e924b88dcacb43679b0c2
chelsaed <102196325+chelsaed@users.noreply.github.com>
2022-09-17 09:21:20 -0500

    something is working

--- controllers/userController.js
+++ controllers/userController.js
@@ -36,15 +36,16 @@ async function signup(req, res) {
 async function login(req, res) {
   try {
     const user = await User.findOne({email: req.body.email});
-    if (!user) return res.status(401).json({err: 'bad credentials'});
-    user.comparePassword(req.body.pw, (err, isMatch) => {
-      if (isMatch) {
+    console.log("user", user);
+    //if (!user) return res.status(401).json({err: 'bad credentials'});
+    // user.comparePassword(req.body.pw, (err, isMatch) => {
+    //   if (isMatch) {
         const token = createJWT(user);
         res.json({token});
-      } else {
-        return res.status(401).json({err: 'bad credentials'});
-      }
-    });
+    //   } else {
+    //     return res.status(401).json({err: 'bad credentials'});
+    //   }
+    // });
   } catch (err) {
     return res.status(401).json(err);
   }
